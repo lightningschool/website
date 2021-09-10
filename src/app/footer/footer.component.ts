bd2f2304ae57a02887a058b22ee5cea6e03abbbc
Anton <anton.harttung@protonmail.ch>
2021-09-10 16:47:55 +0200

    Update footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-license: string='Source code regised under the MIT License and documentation under the CC0 1.0 Universal';
+license: string='Source code regised under the MIT License and documentation under GNU FDL';
 
   constructor() { }
 
